02f627f4f7dc9994374f198fcd19d5ec07a2401f
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2022-07-16 11:33:17 -0300

    added axios request and logging the time of execution

--- requests/src/Requests.jsx
+++ requests/src/Requests.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-function Requests() {
+export default function Requests() {
+
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+
+        const getData = async () => {
+            var t0 = performance.now()
+
+            try {
+                const response = await fetch("https://ghibliapi.herokuapp.com/films");
+                const data = await response.json()
+                setData(data);
+                // console.log(JSON.stringify(data));
+
+            } catch (error) {
+                console.log("Error:", error)
+            }
+
+            var t1 = performance.now()
+            console.log('useFetch normal - Time taken to execute:' + (t1 - t0) + ' milliseconds')
+        }
+        getData();
+
+    }, [])
+
     return (
-        <div>Requests</div>
+        <>
+            <h1>Requests</h1>
+            <br />
+            <br />
+            <br />
+            {data.map((item) => {
+                return (
+                    <div key={item.id}>
+                        <strong>{item.title}</strong>
+                        <p>{item.description}</p>
+                    </div>)
+            })}
+        </>
     )
 }
-
-export default Requests
