baef20db5fd288439d634f18d9f76990dbf8ead4
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2022-08-02 15:29:56 -0300

    finishing deezer api

--- requests/src/components/Deezer.jsx
+++ requests/src/components/Deezer.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 function Deezer() {
+
+    const id = process.env.REACT_APP_DEEZER
+
+    const userUrl = `https://api.deezer.com/user/${id}`
+    const playlistsUrl = `https://api.deezer.com/user/${id}/playlists`
+
+    const [users, setUsers] = useState();
+    const [playlists, setPlaylists] = useState();
+
+    function getData() {
+        axios.get(userUrl, {
+            'Access-Control-Allow-Origin': '*',
+        })
+            .then(response => {
+                // console.log(response)
+                setUsers(response.data)
+            })
+            .catch(error => console.log("Error" + error))
+
+        axios.get(playlistsUrl, {
+            'Access-Control-Allow-Origin': '*',
+        })
+            .then(response => {
+                console.log(response)
+                setPlaylists(response.data.data)
+            })
+            .catch(error => console.log("Error" + error))
+
+    }
     return (
         <>
             <h1>Deezer</h1>
+            <button onClick={() => getData()}>GET User and Playlist</button>
+            {/* {<h1>JSON: </h1> && JSON.stringify(users)} */}
+            <div>
+                {users && (<h2>{users.name} ({users.id})</h2>) &&
+                    (<img src={users.picture_medium} alt={users.name} />)}
+            </div>
+            {playlists && (<div>
+                <h1>TOTAL: {playlists.length}</h1>
+                {playlists?.map(playlist => {
+                    return (
+                        <div key={playlist.id}>
+                            <h2>{playlist.title}</h2>
+                            <p>{playlist.duration} segs</p>
+                        </div>
+                    )
+                })}
+            </div>)}
         </>
     )
 }
